9199bbdcea2026841eba656f51635dd1874bee4f
perpouh <perpouh@gmail.com>
2021-02-17 18:59:34 +0900

    印刷用スタイルとか検索とかソートとか

--- src/components/Sortable.vue
+++ src/components/Sortable.vue
@@ -1,10 +1,32 @@
 <template>
   <div>
-    <input type="search" placeholder="検索">
-    <select>
+    <input type="search" placeholder="検索" v-model="searchWord" @keydown.enter="send">
+    <select v-model="sortKey" @change="sort">
       <option>ソート順</option>
-      <option>時期</option>
-      <option>規模</option>
+      <option value="ym">時期</option>
+      <option value="scale">規模</option>
     </select>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      searchWord: "",
+      sortKey: ""
+    }
+  },
+  methods: {
+    send(event){
+      // 日本語入力中のEnterは無視する
+      if (event.keyCode !== 13) return;
+
+      this.$emit("search", this.searchWord);
+    },
+    sort(){
+      this.$emit("sort", this.sortKey)
+    }
+  }
+}
+</script>
--- src/view/Index.vue
+++ src/view/Index.vue
@@ -1,22 +1,71 @@
 <template>
   <article>
+    <h1>職務経歴書</h1>
     <section>
-      <carrer-table v-for="carrer in resume.carrer" :key="carrer.no" :carrer="carrer"></carrer-table>
+      <h2>自己PR</h2>
+      <div v-html="nl2br(resume.summary)"></div>
+    </section>
+    <section>
+      <h2>技術スタック</h2>
+      <h3>OS</h3>
+      {{resume.skill.os.join(',')}}
+      <h3>言語/フレームワーク</h3>
+      {{resume.skill.language_framework.join(',')}}
+      <h3>ミドルウェア</h3>
+      {{resume.skill.middleware.join(',')}}
+      <h3>ツール</h3>
+      {{resume.skill.tools.join(',')}}
+    </section>
+    <section>
+      <sortable @search="refine" @sort="sort"></sortable>
+    </section>
+    <section>
+      <carrer-table v-for="carrer in disp_carrer" :key="carrer.no" :carrer="carrer"></carrer-table>
+      <p v-if="!carrer">検索結果はありません。</p>
     </section>
   </article>
 </template>
 
 <script>
 import CarrerTable from '../components/CarrerTable'
+import Sortable from '../components/Sortable'
 import resume from '../resume.json'
 export default {
   data(){
     return {
-      resume: resume
+      resume: resume,
+      disp_carrer: resume.carrer
     }
   },
   components: {
-    CarrerTable
-  }
+    CarrerTable,
+    Sortable
+  },
+  methods: {
+    nl2br(text){
+      return text.replace(/\n/g,'<br/>')
+    },
+    refine(word){
+      // あまりちゃんと考えていない
+      if(typeof word != "string") return;
+      this.disp_carrer = resume.carrer.filter(function(carrer){
+        if(carrer.summary.search(word) > 0) return true;
+        if(carrer.role.search(word) > 0) return true;
+        if(carrer.phase_in_charge.search(word) > 0) return true;
+        if(carrer.business_content.join('').search(word) > 0) return true;
+        if(carrer.achievement.search(word) > 0) return true;
+        if(carrer.development_environment.join('').search(word) > 0) return true;
+        return false;
+      });
+    },
+    sort(sortKey){
+      this.disp_carrer = this.disp_carrer.sort(function(a, b){
+        // 規模のところがStringなのでもうちょっと考えないとだめ
+        if(a[sortKey] > b[sortKey])return 1
+        if(a[sortKey] < b[sortKey])return -1
+        return 0
+      })
+    }
+  } 
 }
 </script>
